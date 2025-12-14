1b1e14aa959f295dac25759b4a6f37e4279333c4
Sarah Eliziário de Brito <sarah1202brito@gmail.com>
2025-12-14 18:38:42 -0300

    Add files via upload

---
+++ Python simples/in.py
@@ -0,0 +1,13 @@
+name= input('Qual é o seu nome?')
+print ('Olá', name, 'é um prazer te conhecer!')
+
+idade= input('Qual é a sua idade?')
+print('UAU!', name, 'você Tem', idade, 'anos!' 'logo você é')
+
+if int(idade) >= 18:
+    print ('🍺 Maior de idade')
+else:
+   print('🍼 Menor de idade')
+
+   print('😊Fim do programa.')
+print('🤗 Obrigado por participar do nosso programa,', name, '!')
